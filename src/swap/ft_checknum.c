a532eb6ea1b6955f213ef9a8ade32b0b0e90295a
Diego Moreira Sardinha De Souza <diegoaguia31@gmail.com>
2024-02-01 16:56:08 +0000

    Finish

--- src/swap/ft_checknum.c
+++ src/swap/ft_checknum.c
@@ -63,7 +63,7 @@ int	check_nvalues(char **num, int index)
 		curr = ft_itoa(value);
 		if (value < INT_MIN || value > INT_MAX)
 			free_nvalues(curr);
-		if (num[index][0] == '+')
+		if (num[index][0] == '+' || num[index][0] == '0')
 		{
 			if (ft_strncmp(&num[index][1], curr, ft_strlen(num[index]) - 1))
 				free_nvalues(curr);
